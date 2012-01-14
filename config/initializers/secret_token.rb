60631685de0a7c05e277400faec295fc312cd14e
So Townsend <sotownsend@4g.net>
2012-01-14 16:51:32 -0500

    first commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GFWebsite3::Application.config.secret_token = '918c93bfff004d4e1e58a09ecfb6d3723540f5f1a30db40ace1f90fc6f0c0721965f8a6296d453c814296fff574a314b1ac15f423c4e02e6c2cdf20aacef9385'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GFWebsite3::Application.config.session_store :cookie_store, key: '_GFWebsite3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GFWebsite3::Application.config.session_store :active_record_store
